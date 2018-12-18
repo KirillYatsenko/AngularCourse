cefc094acf9d85c9bccd558cb666373e086d46e3
KirillYatsenko <kirill31051997f@gmail.com>
2018-12-18 07:59:59 +0200

    form-reactive-assignment finished

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  public projectForm: FormGroup;
+  public statuses = [{'id':1, 'name':'Stable'}, {'id':2, 'name': 'Critical'}, {'id':3, 'name': 'Finished'}];
+
+  ngOnInit(): void {
+    this.setUpForm();
+  }
+
+   private setUpForm(): void {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl('', [Validators.required, this.forbiddenName.bind(this)], this.forbiddenNameAsync),
+      mail: new FormControl('', [Validators.required, Validators.email]),
+      status: new FormControl('')
+    });
+  }
+
+  public onSubmit(): void{
+    console.log(this.projectForm.value);
+  }
+
+  private forbiddenName(control: FormControl): {[s:string]: boolean}{
+    if(String(control.value).replace(' ', '').toUpperCase() == "TEST"){
+      return {'projectName':true}
+    }
+
+    return null;
+  }
+  
+  private forbiddenNameAsync(control: FormControl): Promise<any> | Observable<any>{
+    const promise  = new Promise<any>((resolve, reject) => {
+      setTimeout(()=>{
+        if(String(control.value).replace(' ', '').toUpperCase() == "TESTASYNC"){
+          resolve({'projectNameAsync': true});
+        }else{
+          resolve(null);
+        }
+      }, 1500)
+    });
+
+    return promise;
+  }
+  
 }
